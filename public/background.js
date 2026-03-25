55aac78e98d517c79415eb97b26c207b7c867297
Andrew Gabaraev <androgabar@gmail.com>
2026-03-25 14:33:18 +0300

    Background script tab management update

--- public/background.js
+++ public/background.js
@@ -1,3 +1,11 @@
-chrome.action.onClicked.addListener(() => {
-    chrome.tabs.create({ url: 'index.html' });
+chrome.action.onClicked.addListener(async () => {
+    const url = chrome.runtime.getURL('index.html');
+    const tabs = await chrome.tabs.query({ url });
+
+    if (tabs.length > 0) {
+        chrome.tabs.update(tabs[0].id, { active: true });
+        chrome.windows.update(tabs[0].windowId, { focused: true });
+    } else {
+        chrome.tabs.create({ url: 'index.html' });
+    }
 });
